d51ab539f72db2fb7fe46709adc372a3c4e66b64
Yuka Minamizaki <ym431@bath.ac.uk>
2017-12-13 18:27:04 +0000

    adding getImage method

--- src/Backend.java
+++ src/Backend.java
@@ -1,22 +1,31 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
 public class Backend {
 
+	
+	
 	public static void main(String[] args) {
 		Backend main = new Backend();
+		
 		main.getImage();
 		
 	}
 	
-	public void getImage(){
-		try{
-			BufferedImage image = ImageIO.read(new File("/Users/al/some-picture.jpg"));
+	public BufferedImage getImage(){
+		BufferedImage readimage = null;
+		try {
+			readimage = ImageIO.read(new File("C:/Users/yukam/OneDrive/FYPGit/images/receipt4.JPG"));
+			
+			System.out.println("success");
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return readimage;
 	}
 
 }
