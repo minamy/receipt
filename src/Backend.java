da8de87764069fbbfb42b15f98a2a9375a894a08
Yuka Minamizaki <ym431@bath.ac.uk>
2017-12-13 17:13:26 +0000

    adding image folder

--- src/Backend.java
+++ src/Backend.java
@@ -1,9 +1,22 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 public class Backend {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Backend main = new Backend();
+		main.getImage();
+		
+	}
+	
+	public void getImage(){
+		try{
+			BufferedImage image = ImageIO.read(new File("/Users/al/some-picture.jpg"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
